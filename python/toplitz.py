540d95991113a9a25abc312b2e97b45ecb79f6d1
SazidAF <sazidfarhan.a7c@gmail.com>
2022-11-08 10:08:36 +0600

    ToeplitzMatrix https://leetcode.com/problems/toeplitz-matrix/ #1

--- python/toplitz.py
+++ python/toplitz.py
@@ -3,11 +3,19 @@
 
 class Solution:
     def isToeplitzMatrix(self, matrix: list[list[int]]) -> bool:
-        for i,j in enumerate(matrix):
-            print(i)
-            print(j)
-            for k in j:
-                print(k)
+        m = len(matrix)
+        n = len(matrix[0])
+        flag = True
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != matrix[i-1][j-1]:
+                    flag = False
+                    print(flag)
+        if flag:
+            print("true")
+        else:
+            print("false")
+
 
 
 sol = Solution()
