3886f5301c1c8be2e9bd94057130feb3e57c8116
SazidAF <sazidfarhan.a7c@gmail.com>
2023-01-25 12:25:51 +0600

    isIn, removeAllOf, insertAt, size Method Added in LinkedList.py

--- Implementations/python/LinkedListDS/linkedList.py
+++ Implementations/python/LinkedListDS/linkedList.py
@@ -4,21 +4,44 @@ class Node:
     def __init__(self, data = None, next = None):
         self.data = data
         self.next = next
+    def str(self):
+        return f"Append: {self.data}"
+
 
 class linkedList:
     def __init__(self):
         self.head = None
+        self.listSize = 0
 
     def append(self, data):
         newNode = Node(data)
         if self.isEmpty():
             self.head = newNode
+            self.listSize += 1
             return None
         current = self.head
         while current.next:
             current = current.next
         current.next = newNode
-        return None
+        self.listSize += 1
+        print(newNode.str())
+
+
+    def insertAt(self, position, data):
+        newNode = Node(data)
+        if position >= self.size():
+            self.append(data)
+        else:
+            currPos = 0
+            current = self.head
+            while current:
+                if(currPos == position-1):
+                    newNode.next = current.next
+                    current.next = newNode
+                current = current.next
+                currPos += 1
+
+
 
     def remove(self, data):
         current = self.head
@@ -29,13 +52,17 @@ class linkedList:
                 current.next = current.next.next
                 return data
             current = current.next
+            self.listSize -= 1
         return None
 
     def removeFirst(self):
         removedhead = self.head
         self.head = self.head.next
         removedhead.next = None
+        self.listSize -= 1
         return removedhead.data
+
+
 
     def removeTail(self):
         current = self.head
@@ -62,6 +89,9 @@ class linkedList:
         else:
             print("Empty List. Append some data to continue")
 
+    def size(self):
+        return self.listSize
+
     def find(self, data):
         current = self.head
         if self.isEmpty():
@@ -74,16 +104,40 @@ class linkedList:
             position += 1
         return None
 
+    def isIn(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def removeAllOf(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                self.remove(data)
+            current = current.next
+
+    def reverseList(self):
+        pass
 ll = linkedList()
 ll.printList()
-# ll.append(1)
-# ll.append(2)
-# ll.append(3)
-# ll.append(4)
-# ll.append(5)
-# ll.append(6)
-# ll.append(7)
-# ll.printList()
+
+ll.append(0)
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+ll.append(6)
+ll.append(7)
+ll.printList()
+ll.insertAt(11, 3)
+ll.printList()
+ll.removeAllOf(3)
+ll.printList()
+# print(ll.isIn(11))
 # print(f"Popped: {ll.removeTail()}")
 # print(f"removed {ll.remove(2)}")
 # ll.printList()
