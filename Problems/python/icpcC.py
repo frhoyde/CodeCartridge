5ef1ae414e6eba7304936b2dab5a74f53c30477a
SazidAF <sazidfarhan.a7c@gmail.com>
2023-02-16 22:07:35 +0600

    stuff

---
+++ Problems/python/icpcC.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+t = inp()
+i = 1
+while i <= t:
+    i += 1
+    n, k = invr()
+    times = inlt()
+    p1l1 = n
+    emp = [0 for i in range(n+1)]
+    empTime = [0 for i in range(n+1)]
+    time = 0
+    while emp[3] != 2:
+        time += 1
+        if emp[3] =
+        pass
+
+    print(arr)
+    print(p1l1, p2l2)
